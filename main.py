20e603f647ef993c2ec0251a9277bc58edf3029d
Julia Brown <juliacelestebrown@gmail.com>
2021-02-11 13:57:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print('elite qualifier')
+
+
+def is_odd(n):
+    # Function that returns a boolean. True if the number is odd, False if it is even.
+    # return n % 2 > 0
+    if (n % 2 == 1):
+      return True
+    
+    return False
